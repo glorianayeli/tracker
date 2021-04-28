145ac8927b5b4e477f3e64ebdb1c4b49ba2380a8
glorianayeli <glorianayeli_jaime@ucol.mx>
2021-04-28 17:51:59 -0500

    adding delete, reminder, add in json server

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -7,7 +7,7 @@ const Tasks = ({tasks, onDelete, onToggle}) => {
     return (
         <>
          {tasks.map((task) => (
-             <Task key={task.id} task={task} 
+             <Task key={task} task={task} 
              onDelete={onDelete} 
              onToggle={onToggle} />
          ))}   
